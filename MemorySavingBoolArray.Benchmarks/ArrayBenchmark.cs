14238ac9b64c330e887cd62b36b75e7e7e11f358
timmer98 <38386858+timmer98@users.noreply.github.com>
2022-06-13 19:14:05 +0200

    Implemented simple benchmarks; Now trying to optimize

---
+++ MemorySavingBoolArray.Benchmarks/ArrayBenchmark.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace MemorySavingBoolArray.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class ArrayBenchmark
+    {
+        [Params(100, 100_000)]
+        public int Size { get; set; }
+
+        [Benchmark]
+        public void Set_MemorySavingBoolArray()
+        {
+            var array = new MemorySavingBoolArray(this.Size);
+            for (int i = 0; i < this.Size; i++)
+            {
+                array[i] = true;
+            }
+        }
+
+        [Benchmark]
+        public void Set_NormalBoolArray()
+        {
+            var array = new bool[this.Size];
+            for (int i = 0; i < this.Size; i++)
+            {
+                array[i] = true;
+            }
+        }
+
+        [Benchmark]
+        public void Get_MemorySavingBoolArray()
+        {
+            var array = new MemorySavingBoolArray(this.Size);
+            for (int i = 0; i < this.Size; i++)
+            {
+                _ = array[i];
+            }
+        }
+
+        [Benchmark]
+        public void Get_NormalBoolArray()
+        {
+            var array = new bool[this.Size];
+            for (int i = 0; i < this.Size; i++)
+            {
+                _ = array[i];
+            }
+        }
+    }
+}
